6657c80128edaa3a809eafcf0a7460be3afdd54c
Adrian Haugsness <limitx3m@outlook.com>
2022-01-27 16:45:39 -0600

    new idea

--- content.js
+++ content.js
@@ -1,13 +1,62 @@
 console.log("TEST")
 
-document.body.style.backgroundColor = "blue";
+// document.body.style.backgroundColor = "blue";
 
-// let observer = new MutationObserver((mutationRecords) => {
-//  console.log(mutationRecords)
-// });
+// let button = document.createElement('input');
+// button.type = "number";
+// button.value = "Change background to red";
+
+// let child = document.body.firstChild;
+// let swappage = document.getElementById("swap-page");
+// let parent = swappage.parentElement;
+// let next = swappage.nextSibling;
+// let newbutton = document.createElement("button");
+// let text = document.createTextNode("test");
+
+// button.appendChild(text);
+
+// if(next) parent.insertBefore(newbutton, next)
+// else parent.appendChild(button);
+
+
+// document.body.insertBefore(button, child);
+
+// button.addEventListener("click", button_click);
+
+
+
+
+function button_click() {
+  console.log("here");
+  document.body.style.backgroundColor = "blue";
+}
+
+///
 
 const swapInput = document.getElementById("swap-currency-input");
 const swapOutput = document.getElementById("swap-currency-output");
+const swapButton = document.getElementById("swap-button");
+
+const parent = swapButton.parentElement;
+const next = swapButton.nextSibling;
+
+
+let inputAbove = document.createElement("input");
+inputAbove.type = "number";
+inputAbove.id = "above";
+inputAbove.placeholder = "Insert Above Price";
+
+
+let inputBelow = document.createElement("input");
+inputBelow.type = "number";
+inputBelow.id = "below";
+inputBelow.placeholder = "Insert Below Price";
+
+
+
+parent.appendChild(inputAbove);
+parent.appendChild(inputBelow);
+
 
 const test = swapOutput.getElementsByTagName("input")[0];
 
